cc493965a2ee18aea8c1e4fd88167b01a2a6e367
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-04-15 17:22:19 +0300

    bump derive version, test hash explicitly

--- tester/src/test_large_field.rs
+++ tester/src/test_large_field.rs
@@ -11,3 +11,14 @@ fn test_to_hex() {
     assert_eq!("0000000000000000000000000000000000000000000000000000000000000002", to_hex(&a));
     println!("`2` into hex = {}", to_hex(&a));
 }
+
+#[test]
+fn test_hash_impl() {
+    let mut hashset = std::collections::HashSet::new();
+
+    hashset.insert(Fr::from_repr(FrRepr::from(2)).unwrap());
+    hashset.insert(Fr::from_repr(FrRepr::from(4)).unwrap());
+    hashset.insert(Fr::from_repr(FrRepr::from(2)).unwrap());
+
+    assert_eq!(hashset.len(), 2);
+}
