0cf8851a04317a284cb383daea320a20a279aedd
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-04-15 17:24:53 +0300

    revert requiring hash on engine for now

--- src/lib.rs
+++ src/lib.rs
@@ -308,7 +308,7 @@ pub trait PrimeField: Field {
 /// An "engine" is a collection of types (fields, elliptic curve groups, etc.)
 /// with well-defined relationships. Specific relationships (for example, a
 /// pairing-friendly curve) can be defined in a subtrait.
-pub trait ScalarEngine: Sized + 'static + Clone + Copy + std::hash::Hash {
+pub trait ScalarEngine: Sized + 'static + Clone {
     /// This is the scalar field of the engine's groups.
     type Fr: PrimeField + SqrtField;
 }
