6c4b42665cbab6ef33a54854822c78e767fbdfbc
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-06-16 19:43:24 +0300

    engine is send and sync as a marker trait

--- src/lib.rs
+++ src/lib.rs
@@ -308,7 +308,7 @@ pub trait PrimeField: Field {
 /// An "engine" is a collection of types (fields, elliptic curve groups, etc.)
 /// with well-defined relationships. Specific relationships (for example, a
 /// pairing-friendly curve) can be defined in a subtrait.
-pub trait ScalarEngine: Sized + 'static + Clone {
+pub trait ScalarEngine: Sized + 'static + Clone + Send + Sync {
     /// This is the scalar field of the engine's groups.
     type Fr: PrimeField + SqrtField;
 }
